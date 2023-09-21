ef2783eb4cc808e692469c60ce74ca4000fb67a9
Dias <29014732+baiboldy@users.noreply.github.com>
2023-09-21 19:10:45 +0600

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,13 +3,13 @@ import { Navbar } from "../Navbar/Navbar";
 
 export const Layout = ({ children }) => {
   return (
-    <>
+    
       <React.Fragment>
         <div className="container">
           <Navbar />
           {children}
         </div>
       </React.Fragment>
-    </>
+    
   );
 };
